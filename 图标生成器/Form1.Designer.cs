4caf68ffa145a17e99d47058cdb07d2d271d4535
chenyj666 <130724340+chenyj666@users.noreply.github.com>
2023-04-14 11:23:44 +0800

    Add files via upload

---
+++ 图标生成器/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace 图标生成器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(13, 13);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(68, 19);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.Text = "32X32";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(13, 39);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(68, 19);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "64X64";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(13, 65);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(84, 19);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.Text = "128X128";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Checked = true;
+            this.radioButton4.Location = new System.Drawing.Point(13, 91);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(84, 19);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "256X256";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(119, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 37);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "拖放生成";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 119);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "图标生成器";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 图标生成器/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 图标生成器
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            int xx = 0;
+            if (radioButton1.Checked) xx = 32;
+            if (radioButton2.Checked) xx = 64;
+            if (radioButton3.Checked) xx = 128;
+            if (radioButton4.Checked) xx = 256;
+            Size size = new Size(xx, xx);
+            string img1 = s[0];
+            string[] sname=img1.Split('\\');
+            string[] wname = sname[sname.Length - 1].Split('.');
+            //MessageBox.Show(img1);
+            createico(img1, size, wname[0]);
+            label1.Text = "生成成功";
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+
+        }
+        void createico(string img,Size size,string savename)
+        {
+            Image bmico = new Bitmap(new Bitmap(img), size);
+            MemoryStream bitmapstream = new MemoryStream();
+            MemoryStream icostream = new MemoryStream();
+            bmico.Save(bitmapstream, ImageFormat.Png);
+            BinaryWriter binw = new BinaryWriter(icostream);
+            binw.Write((short)0);
+            binw.Write((short)1);
+            binw.Write((short)1);
+            binw.Write((byte)bmico.Width);
+            binw.Write((byte)bmico.Height);
+            binw.Write((short)0);
+            binw.Write((short)0);
+            binw.Write((short)32);
+            binw.Write((int)bitmapstream.Length);
+            binw.Write(22);
+            binw.Write(bitmapstream.ToArray());
+            binw.Flush();
+            binw.Seek(0, SeekOrigin.Begin);
+            string path = System.Environment.CurrentDirectory + "\\" + savename + ".ico";
+            Stream iconfilestream = new FileStream(path, FileMode.Create);
+            Icon ico = new Icon(icostream);
+            ico.Save(iconfilestream);
+            iconfilestream.Close();
+            binw.Close();
+            icostream.Close();
+            bitmapstream.Close();
+            ico.Dispose();
+            bmico.Dispose();
+        }
+
+
+ 
+    }
+}
